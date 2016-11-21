dfeb218837c5f71c3b205e580bfce69ce323b6cd
= <=>
2016-11-21 06:08:04 -0800

    Background process to start sinatra

--- windows/startup.cmd
+++ windows/startup.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser" >> "%TEMP%\StartupLog.txt" 2>&1 
-PowerShell -NoExit start-job -filepath {C:\Users\IEUser\Desktop\browser_controller\startup.ps1} >> "%TEMP%\StartupLog.txt" 2>&1 
+PowerShell C:\Users\IEUser\Desktop\browser_controller\startup.ps1
